e1f7d35858a1c6710af55b2151c2b972767ad101
johncosta2021 <77306740+johncosta2021@users.noreply.github.com>
2022-06-01 20:31:58 -0300

    atualizei

---
+++ notepad/Nova pasta/index.js
@@ -0,0 +1,10 @@
+
+var stripeColor = white;
+drawBox(stripeColor);
+drawBox(stripeColor);
+drawBox(stripeColor);
+newLine();
+stripeColor = red;
+drawBox(stripeColor);
+drawBox(stripeColor);
+drawBox(stripeColor);
